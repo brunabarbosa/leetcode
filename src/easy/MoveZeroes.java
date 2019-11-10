bce720fc685dc592b8f16965fcb65031c373e31a
Bruna <bbdeolive@gmail.com>
2019-11-10 20:44:48 -0300

    done

--- src/easy/MoveZeroes.java
+++ src/easy/MoveZeroes.java
@@ -1,5 +1,26 @@
 package easy;
 
-public class MoveZeroes {
+import java.util.Arrays;
 
+public class MoveZeroes {
+    public int[] moveZeroes(int[] nums) {
+    	for (int i = nums.length - 1; i > 0; i--) {
+    		for (int j = nums.length - 1; j > 0; j--) {
+				if (nums[j - 1] == 0) {
+					swap(j - 1, j, nums);
+				}
+			}
+		}
+    	return nums;
+    }
+    
+    private void swap(int i, int j, int[] nums) {
+    	int aux = nums[i];
+    	nums[i] = nums[j];
+    	nums[j] = aux;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(Arrays.toString(new MoveZeroes().moveZeroes(new int[] {0, 1, 0, 3, 12})));
+	}
 }
