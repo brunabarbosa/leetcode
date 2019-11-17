177d42475e1dd638e72185a68278a49446b3c244
Bruna <bbdeolive@gmail.com>
2019-11-17 09:27:44 -0300

    done

--- src/patterns/sligingwindow/NoRepeatSubstring.java
+++ src/patterns/sligingwindow/NoRepeatSubstring.java
@@ -1,13 +1,39 @@
 package patterns.sligingwindow;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class NoRepeatSubstring {
 	public static int findLength(String str) {
-		// TODO: Write your code here
-		return -1;
+		int windowStart = 0;
+		int max = 0;
+
+		Map<Character, Integer> charFrequencyMap = new HashMap<>();
+
+		for (int windowEnd = 0; windowEnd < str.length(); windowEnd++) {
+			charFrequencyMap.put(str.charAt(windowEnd), charFrequencyMap.getOrDefault(str.charAt(windowEnd), 0) + 1);
+
+			while (charFrequencyMap.get(str.charAt(windowEnd)) > 1) {
+				charFrequencyMap.put(str.charAt(windowStart),
+						charFrequencyMap.getOrDefault(str.charAt(windowStart), 0) - 1);
+
+				if (charFrequencyMap.get(str.charAt(windowStart)) == 0) {
+					charFrequencyMap.remove(str.charAt(windowStart));
+				}
+
+				windowStart++;
+			}
+
+			max = Math.max(max, windowEnd - windowStart + 1);
+
+		}
+
+		return max;
+
 	}
 
 	public static void main(String[] args) {
-		int result = NoRepeatSubstring.findLength("abbbb");
+		int result = NoRepeatSubstring.findLength("aabccbb");
 		System.out.println(result);
 	}
 }
